f0b28aeca8f4fff739cfd54e24280ab836beb4b3
criZorr <crizorr25@gmail.com>
2025-02-10 12:34:37 -0500

    testing PWA

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "v1_cache_genshin_calculator",
+  urlsToCache = [
+    "./",
+    "./assets",
+    "./components",
+    "./db",
+    "./font",
+    "./helpers",
+    "./styles",
+    "./characters.js",
+    "./personal.html",
+    "./personal.js",
+    "./weapons.html",
+    "./weapons.js",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("Fail to cache regist", err))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
