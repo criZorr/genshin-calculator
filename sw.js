bacf3af4295e6a36edfd744ff667dd2aad2afc26
criZorr <crizorr25@gmail.com>
2025-02-11 17:44:47 -0500

    Testing PWA. Adding a <<Settings>> section to import and export the user data

--- sw.js
+++ sw.js
@@ -1,36 +1,60 @@
 const CACHE_NAME = "v1_cache_genshin_calculator",
+  talents_cache = "talents_cache",
   urlsToCache = [
     "./",
-    "./assets",
-    "./components",
-    "./db",
-    "./font",
-    "./helpers",
-    "./styles",
     "./characters.js",
+    "./index.html",
+    "./index.js",
+    "./LICENSE",
+    "./manifest.json",
     "./personal.html",
     "./personal.js",
+    "./README.md",
+    "./sw.js",
     "./weapons.html",
     "./weapons.js",
+    "./components/calculateCharacter.js",
+    "./components/calculateNeeded.js",
+    "./components/calculateWeapon.js",
+    "./components/drawItems.js",
+    "./components/drawTotalExcess.js",
+    "./components/drawTotalItems.js",
+    "./components/getCards.js",
+    "./components/toggleSize.js",
+    "./db/characters.json",
+    "./db/data.js",
+    "./db/weapons.json",
+    "./font/OFL.txt",
+    "./font/Raleway-cyrillic.woff2",
+    "./font/Raleway-cyrillic-ext.woff2",
+    "./font/Raleway-italic-cyrillic.woff2",
+    "./font/Raleway-italic-cyrillic-ext.woff2",
+    "./font/Raleway-italic-latin.woff2",
+    "./font/Raleway-italic-latin-ext.woff2",
+    "./font/Raleway-italic-vietnamese.woff2",
+    "./font/Raleway-latin.woff2",
+    "./font/Raleway-latin-ext.woff2",
+    "./font/Raleway-vietnamese.woff2",
+    "./helpers/calculateTotal.js",
+    "./helpers/createLocal.js",
+    "./helpers/createTotalLocal.js",
+    "./helpers/deleteLocal.js",
+    "./helpers/getData.js",
+    "./helpers/getNumbList.js",
+    "./helpers/handleAscension.js",
+    "./styles/font.css",
+    "./styles/styles-no-nesting.css",
+    "./styles/styles.css",
   ];
-
-// self.addEventListener("install", (e) => {
-//   e.waitUntil(
-//     (async () => {
-//       const cache = await caches.open(CACHE_NAME);
-//       await cache.addAll(urlsToCache);
-//     })()
-//   );
-// });
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
-      })
-      .catch((err) => console.log("Falló registro de cache", err))
+    (async () => {
+      try {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(urlsToCache);
+      } catch (err) {}
+    })()
   );
 });
 
