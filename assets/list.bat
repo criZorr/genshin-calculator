db260252f9a821543f22157758ea4f7c9ece72fc
criZorr <crizorr25@gmail.com>
2025-02-12 15:51:27 -0500

    Adding simple assets to cache PWA

--- assets/list.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setLocal EnableDelayedExpansion
-dir "U:\Trabajos\genshin-calculator\assets\talents" /b /a-d > fileslist.txt
-for /f "tokens=* delims= " %%a in (fileslist.txt) do (
-set /a N+=1
-echo ^"./assets/talents/%%a^",>>outputlist.txt
-)
-del fileslist.txt
