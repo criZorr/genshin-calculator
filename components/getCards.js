3236d0d84b7d0f397ad3c8b6dfdf611f8e9f5b56
criZorr <crizorr25@gmail.com>
2025-02-24 14:52:41 -0500

    Adding Birthday card style when a character turns years old

--- components/getCards.js
+++ components/getCards.js
@@ -16,31 +16,49 @@ export default function getCards(data, $container, path) {
     p.innerHTML = name;
     p.setAttribute("_id", i);
 
-    switch (data[i].quality) {
-      case "5-stars":
-        cardBg.classList.add("five");
-        break;
-      case "4-stars":
-        cardBg.classList.add("four");
-        break;
-      case "3-stars":
-        cardBg.classList.add("three");
-        break;
-      case "2-stars":
-        cardBg.classList.add("two");
-        break;
-      case "1-stars":
-        cardBg.classList.add("one");
-        break;
-      case "5-stars-colab":
-        cardBg.classList.add("colab");
-        break;
+    const date = new Date();
+    let today = String(date.getDate()).padStart(2, "0");
+    today += "-";
+    today += String(date.getMonth() + 1).padStart(2, "0");
+
+    if (today === data[i].birthday) {
+      cardBg.classList.add("birth");
+      cardBg.style.position = "relative";
+      cardBg.innerHTML = `
+        <img class="confeti" src="../assets/birthday-12378_128.gif"/>
+        <img
+          class="card-img"
+          _id="${i}" src="${path}/${name.replaceAll('"', "")}.webp"
+          alt="${name}"
+          style="position: absolute"
+        />`;
+    } else {
+      switch (data[i].quality) {
+        case "5-stars":
+          cardBg.classList.add("five");
+          break;
+        case "4-stars":
+          cardBg.classList.add("four");
+          break;
+        case "3-stars":
+          cardBg.classList.add("three");
+          break;
+        case "2-stars":
+          cardBg.classList.add("two");
+          break;
+        case "1-stars":
+          cardBg.classList.add("one");
+          break;
+        case "5-stars-colab":
+          cardBg.classList.add("colab");
+          break;
+      }
+      cardBg.innerHTML = `<img class="card-img" _id="${i}" src="${path}/${name.replaceAll(
+        '"',
+        ""
+      )}.webp" alt="${name}" />`;
     }
 
-    cardBg.innerHTML = `<img class="card-img" _id="${i}" src="${path}/${name.replaceAll(
-      '"',
-      ""
-    )}.webp" alt="${name}" />`;
     card.appendChild(cardBg);
     card.appendChild(p);
     $container.appendChild(card);
