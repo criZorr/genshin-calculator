41a8f3ef1c381e2039f982f5323ab26c97b4f351
criZorr <83469977+criZorr@users.noreply.github.com>
2025-07-30 20:56:00 -0500

    adding a notification when import or export data

--- settings.js
+++ settings.js
@@ -1,24 +1,56 @@
+import getData from "./helpers/getData.js";
+
 const d = document,
   $importFile = d.getElementById("import"),
   $exportBtn = d.getElementById("export-btn"),
-  $importBtn = d.getElementById("import-btn");
+  $importBtn = d.getElementById("import-btn"),
+  $notification = d.querySelector(".notification-container");
 
 let theme = localStorage.getItem("theme");
+
+let stickersData = await getData("./db/stickers.json"),
+  stickersLength = Object.keys(stickersData).length;
+
+const showNoti = (text) => {
+  let n = Math.floor(Math.random() * stickersLength) + 1,
+    file = stickersData[n];
+
+  $notification.querySelector("p").innerHTML = text;
+  $notification.querySelector("img").src = `./assets/stickers/${file}`;
+
+  $notification.classList.add("transition");
+
+  setTimeout(() => {
+    $notification.classList.remove("transition");
+  }, 2000);
+};
 
 const exportFile = () => {
   const data = {};
 
   data["userInfo"] = JSON.parse(localStorage.getItem("userInfo") || "{}");
-  data["userMaterials"] = JSON.parse(localStorage.getItem("userMaterials") || "{}");
-  data["characterData"] = JSON.parse(localStorage.getItem("characterData") || "{}");
-  data["characterTotal"] = JSON.parse(localStorage.getItem("characterTotal") || "{}");
+  data["userMaterials"] = JSON.parse(
+    localStorage.getItem("userMaterials") || "{}"
+  );
+  data["characterData"] = JSON.parse(
+    localStorage.getItem("characterData") || "{}"
+  );
+  data["characterTotal"] = JSON.parse(
+    localStorage.getItem("characterTotal") || "{}"
+  );
   data["weaponData"] = JSON.parse(localStorage.getItem("weaponData") || "{}");
   data["weaponTotal"] = JSON.parse(localStorage.getItem("weaponTotal") || "{}");
-  data["neededCharacter"] = JSON.parse(localStorage.getItem("neededCharacter") || "{}");
-  data["neededWeapon"] = JSON.parse(localStorage.getItem("neededWeapon") || "{}");
+  data["neededCharacter"] = JSON.parse(
+    localStorage.getItem("neededCharacter") || "{}"
+  );
+  data["neededWeapon"] = JSON.parse(
+    localStorage.getItem("neededWeapon") || "{}"
+  );
   data["neededTotal"] = JSON.parse(localStorage.getItem("neededTotal") || "{}");
   data["total"] = JSON.parse(localStorage.getItem("total") || "{}");
-  data["rawMaterials"] = JSON.parse(localStorage.getItem("rawMaterials") || "{}");
+  data["rawMaterials"] = JSON.parse(
+    localStorage.getItem("rawMaterials") || "{}"
+  );
 
   const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
   const url = URL.createObjectURL(blob);
@@ -28,6 +60,8 @@ const exportFile = () => {
   a.download = "gc-data";
   a.click();
   a.remove();
+
+  showNoti("Exported!");
 };
 
 const writeData = (data) => {
@@ -37,35 +71,63 @@ const writeData = (data) => {
     localStorage.setItem("userInfo", JSON.stringify(dataObject["userInfo"]));
 
   if (dataObject["userMaterials"])
-    localStorage.setItem("userMaterials", JSON.stringify(dataObject["userMaterials"]));
+    localStorage.setItem(
+      "userMaterials",
+      JSON.stringify(dataObject["userMaterials"])
+    );
 
   if (dataObject["characterData"])
-    localStorage.setItem("characterData", JSON.stringify(dataObject["characterData"]));
+    localStorage.setItem(
+      "characterData",
+      JSON.stringify(dataObject["characterData"])
+    );
 
   if (dataObject["characterTotal"])
-    localStorage.setItem("characterTotal", JSON.stringify(dataObject["characterTotal"]));
+    localStorage.setItem(
+      "characterTotal",
+      JSON.stringify(dataObject["characterTotal"])
+    );
 
   if (dataObject["weaponData"])
-    localStorage.setItem("weaponData", JSON.stringify(dataObject["weaponData"]));
+    localStorage.setItem(
+      "weaponData",
+      JSON.stringify(dataObject["weaponData"])
+    );
 
   if (dataObject["weaponTotal"])
-    localStorage.setItem("weaponTotal", JSON.stringify(dataObject["weaponTotal"]));
+    localStorage.setItem(
+      "weaponTotal",
+      JSON.stringify(dataObject["weaponTotal"])
+    );
 
   if (dataObject["neededCharacter"])
-    localStorage.setItem("neededCharacter", JSON.stringify(dataObject["neededCharacter"]));
+    localStorage.setItem(
+      "neededCharacter",
+      JSON.stringify(dataObject["neededCharacter"])
+    );
 
   if (dataObject["neededWeapon"])
-    localStorage.setItem("neededWeapon", JSON.stringify(dataObject["neededWeapon"]));
+    localStorage.setItem(
+      "neededWeapon",
+      JSON.stringify(dataObject["neededWeapon"])
+    );
 
   if (dataObject["neededTotal"])
-    localStorage.setItem("neededTotal", JSON.stringify(dataObject["neededTotal"]));
+    localStorage.setItem(
+      "neededTotal",
+      JSON.stringify(dataObject["neededTotal"])
+    );
 
-  if (dataObject["total"]) localStorage.setItem("total", JSON.stringify(dataObject["total"]));
+  if (dataObject["total"])
+    localStorage.setItem("total", JSON.stringify(dataObject["total"]));
 
   if (dataObject["rawMaterials"])
-    localStorage.setItem("rawMaterials", JSON.stringify(dataObject["rawMaterials"]));
+    localStorage.setItem(
+      "rawMaterials",
+      JSON.stringify(dataObject["rawMaterials"])
+    );
 
-  console.log("done");
+  showNoti("Imported!");
 };
 
 const handleFile = (input) => {
