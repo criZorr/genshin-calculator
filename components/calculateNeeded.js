f842c3771af279da0ec1ee7850d35d935d1166df
criZorr <83469977+criZorr@users.noreply.github.com>
2025-10-01 10:45:54 -0500

    fixing calculation

--- components/calculateNeeded.js
+++ components/calculateNeeded.js
@@ -4,7 +4,10 @@ import calculateTotal from "../helpers/calculateTotal.js";
 const weaponMaterials = calculateData("weaponMaterials"),
   stoneMaterials = calculateData("stones"),
   talentMaterials = calculateData("talentMaterials"),
-  enemiesMaterials = [...calculateData("commonEnemies"), ...calculateData("eliteEnemies")],
+  enemiesMaterials = [
+    ...calculateData("commonEnemies"),
+    ...calculateData("eliteEnemies"),
+  ],
   expMaterials = [
     "Wanderer's Advice",
     "Adventurer's Experience",
@@ -40,10 +43,11 @@ export default function calculateNeeded(userLocal, variable) {
     weaponCrafting = $activeTalent["craftingWeapons"],
     materialCrafting = $activeTalent["craftingMaterials"];
 
-  let newUserData = {};
-  let neededData = {};
-
-  let tempObj = {};
+  let newUserData = {},
+    neededData = {},
+    tempObj = {},
+    neededRaw = {},
+    tempRaw = {};
 
   all.forEach((e) => {
     if (!expMaterials.includes(e)) {
@@ -51,9 +55,16 @@ export default function calculateNeeded(userLocal, variable) {
         newUserData[e] = 0;
         neededData[e] = $data[e];
       } else {
-        let calc = ($data[e] || 0) - (($userData[e] || 0) + (tempObj[e] || 0));
+        let calc = ($data[e] || 0) - (($userData[e] || 0) + (tempObj[e] || 0)),
+          calcRaw = ($data[e] || 0) - (($userData[e] || 0) + (tempRaw[e] || 0));
+
+        console.log(e, calc, calcRaw);
+
         if (calc > 0) {
           neededData[e] = calc;
+          calcRaw < 0
+            ? (neededRaw[e] = -1 * calcRaw)
+            : (neededRaw[e] = calcRaw);
         }
 
         if (calc < 0 && enemiesMaterials.includes(e)) {
@@ -61,7 +72,7 @@ export default function calculateNeeded(userLocal, variable) {
           if (maxMaterials.includes(e)) calculate = false;
 
           if (calculate) {
-            calc = -1 * calc;
+            calc *= -1;
 
             if (materialCrafting === 0) calc = Math.floor(calc / 3);
             if (materialCrafting === 1) calc = Math.floor(1.1 * (calc / 3));
@@ -77,7 +88,7 @@ export default function calculateNeeded(userLocal, variable) {
           if (maxMaterials.includes(e)) calculate = false;
 
           if (calculate) {
-            calc = -1 * calc;
+            calc *= -1;
 
             if (talentCrafting === 0) calc = Math.floor(calc / 3);
             if (talentCrafting === 1) calc = Math.floor(1.1 * (calc / 3));
@@ -88,12 +99,29 @@ export default function calculateNeeded(userLocal, variable) {
           }
         }
 
+        if (calcRaw < 0 && talentMaterials.includes(e)) {
+          let calculate = true;
+
+          if (maxMaterials.includes(e)) calculate = false;
+
+          if (calculate) {
+            calcRaw *= -1;
+
+            if (talentCrafting === 0) calcRaw = calcRaw / 3;
+            if (talentCrafting === 1) calcRaw = 1.1 * (calcRaw / 3);
+            if (talentCrafting === 2) calcRaw = (4 / 3) * (calcRaw / 3);
+
+            let position = craftMaterials.indexOf(e);
+            if (position >= 0) tempRaw[craftMaterials[position + 1]] = calcRaw;
+          }
+        }
+
         if (calc < 0 && weaponMaterials.includes(e)) {
           let calculate = true;
           if (maxMaterials.includes(e)) calculate = false;
 
           if (calculate) {
-            calc = -1 * calc;
+            calc *= -1;
 
             if (weaponCrafting === 0) calc = Math.floor(calc / 3);
             if (weaponCrafting === 1) calc = Math.floor(1.1 * (calc / 3));
@@ -104,12 +132,29 @@ export default function calculateNeeded(userLocal, variable) {
           }
         }
 
+        if (calcRaw < 0 && weaponMaterials.includes(e)) {
+          let calculate = true;
+
+          if (maxMaterials.includes(e)) calculate = false;
+
+          if (calculate) {
+            calcRaw *= -1;
+
+            if (materialCrafting === 0) calcRaw = calcRaw / 3;
+            if (materialCrafting === 1) calcRaw = 1.1 * (calcRaw / 3);
+            if (materialCrafting === 2) calcRaw = (4 / 3) * (calcRaw / 3);
+
+            let position = craftMaterials.indexOf(e);
+            if (position >= 0) tempRaw[craftMaterials[position + 1]] = calcRaw;
+          }
+        }
+
         if (calc < 0 && stoneMaterials.includes(e)) {
           let calculate = true;
           if (maxMaterials.includes(e)) calculate = false;
 
           if (calculate) {
-            calc = -1 * calc;
+            calc *= -1;
             calc = Math.floor(calc / 3);
 
             let position = craftMaterials.indexOf(e);
@@ -119,27 +164,39 @@ export default function calculateNeeded(userLocal, variable) {
       }
     }
   });
+
+  tempRaw = {};
   tempObj = {};
 
   expMaterials.forEach((e) => {
     if (!$userData.hasOwnProperty(e)) newUserData[e] = 0;
   });
 
-  let fstBook = ($data["Wanderer's Advice"] || 0) - ($userData["Wanderer's Advice"] || 0),
-    sndBook = ($data["Adventurer's Experience"] || 0) - ($userData["Adventurer's Experience"] || 0),
+  let fstBook =
+      ($data["Wanderer's Advice"] || 0) - ($userData["Wanderer's Advice"] || 0),
+    sndBook =
+      ($data["Adventurer's Experience"] || 0) -
+      ($userData["Adventurer's Experience"] || 0),
     trdBook = ($data["Hero's Wit"] || 0) - ($userData["Hero's Wit"] || 0),
     totalBook = fstBook + 5 * sndBook + 20 * trdBook;
 
   if (totalBook > 0) {
-    if (Math.floor(totalBook / 20) > 0) neededData["Hero's Wit"] = Math.floor(totalBook / 20);
+    if (Math.floor(totalBook / 20) > 0)
+      neededData["Hero's Wit"] = Math.floor(totalBook / 20);
     if (Math.floor((totalBook % 20) / 5) > 0)
       neededData["Adventurer's Experience"] = Math.floor((totalBook % 20) / 5);
-    if ((totalBook % 20) % 5 > 0) neededData["Wanderer's Advice"] = (totalBook % 20) % 5;
+    if ((totalBook % 20) % 5 > 0)
+      neededData["Wanderer's Advice"] = (totalBook % 20) % 5;
   }
 
-  let fstOre = ($data["Enhancement Ore"] || 0) - ($userData["Enhancement Ore"] || 0),
-    sndOre = ($data["Fine Enhancement Ore"] || 0) - ($userData["Fine Enhancement Ore"] || 0),
-    trdOre = ($data["Mystic Enhancement Ore"] || 0) - ($userData["Mystic Enhancement Ore"] || 0),
+  let fstOre =
+      ($data["Enhancement Ore"] || 0) - ($userData["Enhancement Ore"] || 0),
+    sndOre =
+      ($data["Fine Enhancement Ore"] || 0) -
+      ($userData["Fine Enhancement Ore"] || 0),
+    trdOre =
+      ($data["Mystic Enhancement Ore"] || 0) -
+      ($userData["Mystic Enhancement Ore"] || 0),
     totalOre = fstOre + 5 * sndOre + 25 * trdOre;
 
   if (totalOre > 0) {
@@ -147,14 +204,17 @@ export default function calculateNeeded(userLocal, variable) {
       neededData["Mystic Enhancement Ore"] = Math.floor(totalOre / 25);
     if (Math.floor((totalOre % 25) / 5) > 0)
       neededData["Fine Enhancement Ore"] = Math.floor((totalOre % 25) / 5);
-    if ((totalOre % 25) % 5 > 0) neededData["Enhancement Ore"] = (totalOre % 25) % 5;
+    if ((totalOre % 25) % 5 > 0)
+      neededData["Enhancement Ore"] = (totalOre % 25) % 5;
   }
 
   if (variable === "characterTotal")
     localStorage.setItem("neededCharacter", JSON.stringify(neededData));
 
-  if (variable === "weaponTotal") localStorage.setItem("neededWeapon", JSON.stringify(neededData));
-  if (variable === "total") localStorage.setItem("neededTotal", JSON.stringify(neededData));
+  if (variable === "weaponTotal")
+    localStorage.setItem("neededWeapon", JSON.stringify(neededData));
+  if (variable === "total")
+    localStorage.setItem("neededTotal", JSON.stringify(neededData));
 
   let totalUserData = calculateTotal($userData, newUserData);
 
@@ -162,26 +222,39 @@ export default function calculateNeeded(userLocal, variable) {
   let object = {};
 
   for (let i = 0; i < craftMaterialsThree.length; i += 3) {
-    let raw = 0;
-    let raw10 = 0;
-    let raw25 = 0;
+    let raw = 0,
+      raw10 = 0,
+      raw25 = 0;
 
-    if (neededData[craftMaterialsThree[i]] >= 0) raw += neededData[craftMaterialsThree[i]];
+    if (neededRaw[craftMaterialsThree[i]] >= 0)
+      raw += neededRaw[craftMaterialsThree[i]];
 
-    if (neededData[craftMaterialsThree[i + 1]] >= 0)
-      raw += neededData[craftMaterialsThree[i + 1]] * 3;
+    if (neededRaw[craftMaterialsThree[i + 1]] >= 0)
+      raw += neededRaw[craftMaterialsThree[i + 1]] * 3;
 
-    if (neededData[craftMaterialsThree[i + 2]] >= 0)
-      raw += neededData[craftMaterialsThree[i + 2]] * 9;
+    if (neededRaw[craftMaterialsThree[i + 2]] >= 0)
+      raw += neededRaw[craftMaterialsThree[i + 2]] * 9;
 
     if (raw > 0) {
-      let gold10 = Math.ceil((neededData[craftMaterialsThree[i + 2]] || 0) * (9 / 1.21)),
-        purple10 = Math.ceil((neededData[craftMaterialsThree[i + 1]] || 0) * (3 / 1.1)),
-        gold25 = Math.ceil((neededData[craftMaterialsThree[i + 2]] || 0) * (9 / (16 / 9))),
-        purple25 = Math.ceil((neededData[craftMaterialsThree[i + 1]] || 0) * (3 / (4 / 3)));
+      let gold10 = Math.ceil(
+          (neededRaw[craftMaterialsThree[i + 2]] || 0) * (9 / 1.21)
+        ),
+        purple10 = Math.ceil(
+          (neededRaw[craftMaterialsThree[i + 1]] || 0) * (3 / 1.1)
+        ),
+        gold25 = Math.ceil(
+          (neededRaw[craftMaterialsThree[i + 2]] || 0) * (9 / (16 / 9))
+        ),
+        purple25 = Math.ceil(
+          (neededRaw[craftMaterialsThree[i + 1]] || 0) * (3 / (4 / 3))
+        );
 
-      raw10 = Math.ceil((neededData[craftMaterialsThree[i]] || 0) + gold10 + purple10);
-      raw25 = Math.ceil((neededData[craftMaterialsThree[i]] || 0) + gold25 + purple25);
+      raw10 = Math.ceil(
+        (neededRaw[craftMaterialsThree[i]] || 0) + gold10 + purple10
+      );
+      raw25 = Math.ceil(
+        (neededRaw[craftMaterialsThree[i]] || 0) + gold25 + purple25
+      );
 
       object[craftMaterialsThree[i]] = [raw, raw10, raw25];
       object[craftMaterialsThree[i + 1]] = [raw, raw10, raw25];
@@ -190,31 +263,48 @@ export default function calculateNeeded(userLocal, variable) {
   }
 
   for (let i = 0; i < craftMaterialsFour.length; i += 4) {
-    let raw = 0;
-    let raw10 = 0;
-    let raw25 = 0;
+    let raw = 0,
+      raw10 = 0,
+      raw25 = 0;
 
-    if (neededData[craftMaterialsFour[i]] >= 0) raw += neededData[craftMaterialsFour[i]];
+    if (neededRaw[craftMaterialsFour[i]] >= 0)
+      raw += neededRaw[craftMaterialsFour[i]];
 
-    if (neededData[craftMaterialsFour[i + 1]] >= 0)
-      raw += neededData[craftMaterialsFour[i + 1]] * 3;
+    if (neededRaw[craftMaterialsFour[i + 1]] >= 0)
+      raw += neededRaw[craftMaterialsFour[i + 1]] * 3;
 
-    if (neededData[craftMaterialsFour[i + 2]] >= 0)
-      raw += neededData[craftMaterialsFour[i + 2]] * 9;
+    if (neededRaw[craftMaterialsFour[i + 2]] >= 0)
+      raw += neededRaw[craftMaterialsFour[i + 2]] * 9;
 
-    if (neededData[craftMaterialsFour[i + 3]] >= 0)
-      raw += neededData[craftMaterialsFour[i + 3]] * 27;
+    if (neededRaw[craftMaterialsFour[i + 3]] >= 0)
+      raw += neededRaw[craftMaterialsFour[i + 3]] * 27;
 
     if (raw > 0) {
-      let gold10 = Math.ceil((neededData[craftMaterialsFour[i + 3]] || 0) * (27 / 1.331)),
-        purple10 = Math.ceil((neededData[craftMaterialsFour[i + 2]] || 0) * (9 / 1.21)),
-        blue10 = Math.ceil((neededData[craftMaterialsFour[i + 1]] || 0) * (3 / 1.1)),
-        gold25 = Math.ceil((neededData[craftMaterialsFour[i + 3]] || 0) * (27 / (64 / 27))),
-        purple25 = Math.ceil((neededData[craftMaterialsFour[i + 2]] || 0) * (9 / (16 / 9))),
-        blue25 = Math.ceil((neededData[craftMaterialsFour[i + 1]] || 0) * (3 / (4 / 3)));
+      let gold10 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 3]] || 0) * (27 / 1.331)
+        ),
+        purple10 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 2]] || 0) * (9 / 1.21)
+        ),
+        blue10 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 1]] || 0) * (3 / 1.1)
+        ),
+        gold25 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 3]] || 0) * (27 / (64 / 27))
+        ),
+        purple25 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 2]] || 0) * (9 / (16 / 9))
+        ),
+        blue25 = Math.ceil(
+          (neededRaw[craftMaterialsFour[i + 1]] || 0) * (3 / (4 / 3))
+        );
 
-      raw10 = Math.ceil((neededData[craftMaterialsFour[i]] || 0) + gold10 + purple10 + blue10);
-      raw25 = Math.ceil((neededData[craftMaterialsFour[i]] || 0) + gold25 + purple25 + blue25);
+      raw10 = Math.ceil(
+        (neededRaw[craftMaterialsFour[i]] || 0) + gold10 + purple10 + blue10
+      );
+      raw25 = Math.ceil(
+        (neededRaw[craftMaterialsFour[i]] || 0) + gold25 + purple25 + blue25
+      );
 
       object[craftMaterialsFour[i]] = [raw, raw10, raw25];
       object[craftMaterialsFour[i + 1]] = [raw, raw10, raw25];
